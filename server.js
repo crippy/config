b6e0599cf1612e08fc9aac302e46a7df23a7417c
Crippy <marks-c1@email.ulster.ac.uk>
2019-03-07 23:31:34 +0000

    master: ignore config folder

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
+
+// DB Config
+const db = keys.mongoURI;
+// PORT
 const port = process.env.PORT || 5000;
+// connect db
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Mongoo Connected');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello');
